e73be147e2f0732bf35066c6f97a35c53cc8827f
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-05-12 10:59:51 -0300

    Struct de Pedido

---
+++ include/Pedido.hpp
@@ -0,0 +1,14 @@
+#ifndef PEDIDO_HPP
+#define PEDIDO_HPP
+
+#include "Produto.hpp"
+#include "Usuario.hpp"
+#include <istream>
+
+struct Pedido {
+  int = ID std::string = created_at std::string = codigo_rastreio
+
+  Pedido(Usuario x, Carrinho y);
+};
+
+#endif
